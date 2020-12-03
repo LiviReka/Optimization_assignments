70265fc160b20186d8f98d43966cc4a2a766b9f1
Polina Berenstein <polinabee@users.noreply.github.com>
2020-12-03 15:29:43 -0800

    Delete rtf_to_df.py

--- rtf_to_df.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-synthetic = pd.read_csv('synthetic.txt', sep="    ", header=None)
-synthetic.columns = ["a", "b"]
-
-thyroid = pd.read_csv('thyroid.txt', sep="   ", header=None)
-thyroid.columns = ["a", "b",'c','d','e']
-
-thyroid
